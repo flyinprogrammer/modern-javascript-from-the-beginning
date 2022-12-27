a50fd0f47a4f87bf706ee8e644a19d9468161f20
Alan Scherger <alan.scherger@gmail.com>
2022-12-27 20:32:06 +0000

    Type Converstion

--- app.js
+++ app.js
@@ -1,31 +1,115 @@
-// primative types
+let val;
 
-const objectName = 'John Doe';
-console.log(typeof objectName);
+// Number to string
 
-const age = 45;
-console.log(typeof age);
+val = 5;
 
-const hasKids = true;
-console.log(typeof hasKids);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
 
-const car = null;
-console.log(typeof car);
+val = String(555);
 
-let test;
-console.log(typeof test);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
 
-const sym = Symbol();
-console.log(typeof sym);
+val = String(4 + 4);
 
-const hobbies = ['movie', 'music'];
-console.log(typeof hobbies);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
 
-const address = {
-  city: 'Boston',
-  state: 'MA'
-}
-console.log(typeof address);
+val = String(true);
 
-const today = new Date();
-console.log(typeof today);
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+val = String(new Date());
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+
+val = String([1, 2, 3, 4]);
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+
+val = (5).toString();
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+
+val = (true).toString();
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+val = 5;
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+// Length only works on strings
+// console.log(val.length);
+
+val = Number('5');
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = Number(true);
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+
+val = Number(null);
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = Number('hello');
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = Number([1, 2, 3])
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = parseInt('1000');
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = parseInt(1000.3)
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = parseFloat('100.30');
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+
+const val1 = 5;
+const val2 = 6;
+const sum = val1 + val2;
+console.log(sum);
+console.log(typeof sum);
+
+const val3 = String(5);
+const val4 = 6;
+const sum2 = Number(val3 + val4);
+console.log(sum2);
+console.log(typeof sum2);
+
