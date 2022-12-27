6e6f9eed556a0546ef721f22a4d6878cde72092e
Alan Scherger <alan.scherger@gmail.com>
2022-12-27 20:41:20 +0000

    Numbers & the Math Object

--- app.js
+++ app.js
@@ -1,115 +1,35 @@
+const num1 = 100;
+const num2 = 50;
 let val;
 
-// Number to string
-
-val = 5;
-
+val = num1 + num2;
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
 
-val = String(555);
-
+val = num1 - num2;
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
 
-val = String(4 + 4);
-
+val = num1 * num2;
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
 
-val = String(true);
-
+val = num1 / num2;
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
 
-val = String(new Date());
-
+val = num1 % num2;
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
 
-
-val = String([1, 2, 3, 4]);
-
+val = Math.PI
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
 
-
-val = (5).toString();
-
+val = Math.E
 console.log(val);
-console.log(typeof val);
-console.log(val.length);
 
-
-val = (true).toString();
-console.log(val);
-console.log(typeof val);
-console.log(val.length);
-
-val = 5;
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-// Length only works on strings
-// console.log(val.length);
-
-val = Number('5');
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-val = Number(true);
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-
-val = Number(null);
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-val = Number('hello');
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-val = Number([1, 2, 3])
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-val = parseInt('1000');
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-val = parseInt(1000.3)
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-val = parseFloat('100.30');
-console.log(val);
-console.log(typeof val);
-console.log(val.toFixed(2));
-
-
-const val1 = 5;
-const val2 = 6;
-const sum = val1 + val2;
-console.log(sum);
-console.log(typeof sum);
-
-const val3 = String(5);
-const val4 = 6;
-const sum2 = Number(val3 + val4);
-console.log(sum2);
-console.log(typeof sum2);
-
+console.log(Math.round(2.4));
+console.log(Math.ceil(2.4));
+console.log(Math.floor(2.8));
+console.log(Math.sqrt(64));
+console.log(Math.abs(-3));
+console.log(Math.pow(8, 2));
+console.log(Math.min(1, 2, 3, 4));
+console.log(Math.max(1, 2, 3));
+console.log(Math.max(...[4, 5, 6]));
+console.log(Math.floor(Math.random() * 19))
