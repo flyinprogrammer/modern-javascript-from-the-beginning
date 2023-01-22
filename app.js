1c0d483d57c6c68005bd6b43b53980aa0c06fc5c
Alan Scherger <alan.scherger@gmail.com>
2023-01-22 18:25:45 +0000

    we did String Methods & Concatenation

--- app.js
+++ app.js
@@ -1,35 +1,82 @@
-const num1 = 100;
-const num2 = 50;
+const firstName = 'William';
+const lastName = 'Johnson';
+const age = 36;
+
+const str = 'Hello there my name is Brad';
+const tags = 'web design,web development,programming'
+
 let val;
 
-val = num1 + num2;
+val = firstName + lastName;
 console.log(val);
 
-val = num1 - num2;
+val = 'Brad ';
+val += 'Traversy';
 console.log(val);
 
-val = num1 * num2;
+val = 'Hello, my name is' + firstName + ' and I am ' + age;
 console.log(val);
 
-val = num1 / num2;
+// Escaping
+val = 'That\'s awesome, I can\'t wait';
 console.log(val);
 
-val = num1 % num2;
+// Length
+val = firstName.length;
 console.log(val);
 
-val = Math.PI
+// concat()
+val = firstName.concat(' ', lastName);
 console.log(val);
 
-val = Math.E
+val = val.concat(' -- ', 'believe in magic!')
 console.log(val);
 
-console.log(Math.round(2.4));
-console.log(Math.ceil(2.4));
-console.log(Math.floor(2.8));
-console.log(Math.sqrt(64));
-console.log(Math.abs(-3));
-console.log(Math.pow(8, 2));
-console.log(Math.min(1, 2, 3, 4));
-console.log(Math.max(1, 2, 3));
-console.log(Math.max(...[4, 5, 6]));
-console.log(Math.floor(Math.random() * 19))
+//change case
+
+val = firstName.toUpperCase();
+console.log(val);
+
+val = firstName.toLowerCase();
+console.log(val);
+
+val = firstName[2];
+console.log(val);
+
+// indexOf()
+val = firstName.indexOf('l');
+console.log(val);
+
+val = firstName.lastIndexOf('l');
+console.log(val);
+
+// charAt()
+val = firstName.charAt(2);
+console.log(val);
+
+val = firstName.charAt(firstName.length - 1);
+console.log(val);
+
+// substring
+val = firstName.substring(0, 4);
+console.log(val);
+
+val = firstName.substring(0, -1);
+console.log(val);
+
+val = firstName.substring(-1, 3)
+console.log(val);
+
+// split
+val = str.split(' ');
+console.log(val);
+val = tags.split(',');
+console.log(val);
+
+// replace
+val = str.replace('Brad', 'Jack');
+console.log(val);
+
+// includes()
+val = str.includes('foo');
+console.log(val);
