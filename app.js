a1428af0426290c30ef3b0b8aac2c76c91666830
Alan Scherger <alan.scherger@gmail.com>
2023-01-25 02:36:08 +0000

    Template Literals

--- app.js
+++ app.js
@@ -1,82 +1,39 @@
-const firstName = 'William';
-const lastName = 'Johnson';
-const age = 36;
+const firstName = 'John';
+const age = 31;
+const job = 'Web Developer';
+const city = 'Miami';
+let html;
 
-const str = 'Hello there my name is Brad';
-const tags = 'web design,web development,programming'
+// Without template strings (es5)
+html = '<ul><li>Name: ' + firstName + '</li><li>Age: ' + age + ' </li><li>Job: ' + job + ' </li><li>City: ' + city + ' </li></ul>';
 
-let val;
+html = '<ul>' +
+  '<li>Name: ' + firstName + '</li>' +
+  '<li>Age: ' + age + '</li>' +
+  '<li>Job: ' + job + '</li>' +
+  '<li>City: ' + city + '</li>' +
+  '</ul>';
 
-val = firstName + lastName;
-console.log(val);
+function hello() {
+  return "Hello World";
+}
 
-val = 'Brad ';
-val += 'Traversy';
-console.log(val);
+// With template strings (es6)
+html = `
+  <ul>
+    <li>Name: ${firstName}</li>
+    <li>Age: ${age}</li>
+    <li>Job: ${job}</li>
+    <li>City: ${city}</li>
+    <li>${2 + 2}</li>
+    <li>${hello()}</li>
+    <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+  </ul>
+`;
 
-val = 'Hello, my name is' + firstName + ' and I am ' + age;
-console.log(val);
+let foo = 'wow the best foo'
+html += `
+<p>${foo}</p>
+`
 
-// Escaping
-val = 'That\'s awesome, I can\'t wait';
-console.log(val);
-
-// Length
-val = firstName.length;
-console.log(val);
-
-// concat()
-val = firstName.concat(' ', lastName);
-console.log(val);
-
-val = val.concat(' -- ', 'believe in magic!')
-console.log(val);
-
-//change case
-
-val = firstName.toUpperCase();
-console.log(val);
-
-val = firstName.toLowerCase();
-console.log(val);
-
-val = firstName[2];
-console.log(val);
-
-// indexOf()
-val = firstName.indexOf('l');
-console.log(val);
-
-val = firstName.lastIndexOf('l');
-console.log(val);
-
-// charAt()
-val = firstName.charAt(2);
-console.log(val);
-
-val = firstName.charAt(firstName.length - 1);
-console.log(val);
-
-// substring
-val = firstName.substring(0, 4);
-console.log(val);
-
-val = firstName.substring(0, -1);
-console.log(val);
-
-val = firstName.substring(-1, 3)
-console.log(val);
-
-// split
-val = str.split(' ');
-console.log(val);
-val = tags.split(',');
-console.log(val);
-
-// replace
-val = str.replace('Brad', 'Jack');
-console.log(val);
-
-// includes()
-val = str.includes('foo');
-console.log(val);
+document.body.innerHTML = html;
