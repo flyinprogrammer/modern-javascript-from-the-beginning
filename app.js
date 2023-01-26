5b2c12d35002d8a551d7b8e5d89131b946abd382
Alan Scherger <alan.scherger@gmail.com>
2023-01-26 22:36:18 +0000

    Dates and Times

--- app.js
+++ app.js
@@ -1,69 +1,41 @@
 let header = document.getElementById('header');
-header.innerHTML = "Object Literals";
-
-const person = {
-  firstName: 'Steve',
-  lastName: 'Smith',
-  age: 36,
-  email: 'steve@aol.com',
-  hobbies: ['music', 'sports'],
-  address: {
-    city: 'Miami',
-    state: 'FL',
-  },
-  getBirthYear: function() {
-    let todaysYear = new Date().getFullYear();
-    return todaysYear - this.age;
-  }
-}
+header.innerHTML = "Dates and Times";
 
 let val;
 
-val = person.firstName;
-console.log(val);
+const today = new Date();
 
-val = person['lastName'];
-console.log(val);
+val = today;
+console.log(typeof val);
 
-val = person.age;
-console.log(val);
+val = today.toString();
+console.log(typeof val);
 
-val = person.hobbies[1];
-console.log(val);
+const birthday = new Date('9-10-1981 11:24:00');
+console.log(birthday);
 
-val = person.address.state;
-console.log(val);
+let otherBirthday = new Date('September 10 1981');
+console.log(otherBirthday);
 
-val = person.address['city'];
-console.log(val);
+console.log(today.getMonth());
+console.log(today.getDate());
+console.log(today.getDay());
+console.log(today.getFullYear());
+console.log(today.getHours());
+console.log(today.getMinutes());
+console.log(today.getSeconds());
+console.log(today.getMilliseconds());
+console.log(today.getTime());
 
-val = person.getBirthYear();
-console.log(val);
-
-const people = [
-  { name: 'John', age: 30 },
-  { name: 'Mike', age: 23 },
-  { name: 'Nancy', age: 40 },
-]
-
-console.log("for/i")
-for (let i = 0; i < people.length; i++) {
-  console.log("\t" + people[i].name)
-}
-
-
-// extra credit i couldn't help myself
-console.log("for/of")
-for (p of people) {
-  console.log("\t" + p.name);
-}
-
-console.log("for/in");
-for (let x in people) {
-  console.log("\t" + people[x].name);
-}
-
-console.log("forEach");
-people.forEach(function(p) {
-  console.log("\t" + p.name);
-})
+let modBirthday = new Date('9/10/1981');
+console.log("modBirthday: " + modBirthday);
+modBirthday.setMonth(2);
+console.log("modBirthday: " + modBirthday);
+modBirthday.setDate(12);
+console.log("modBirthday: " + modBirthday);
+modBirthday.setFullYear(1985);
+console.log("modBirthday: " + modBirthday);
+modBirthday.setMinutes(30);
+console.log("modBirthday: " + modBirthday);
+modBirthday.setSeconds(25);
+console.log("modBirthday: " + modBirthday);
