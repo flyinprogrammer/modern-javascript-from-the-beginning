5e516c8f0226bb7ec1903541fe3608505f5d8a52
Alan Scherger <alan.scherger@gmail.com>
2023-01-28 21:42:59 +0000

    If Statements & Comparison Operators

--- app.js
+++ app.js
@@ -1,41 +1,99 @@
 let header = document.getElementById('header');
-header.innerHTML = "Dates and Times";
+header.innerHTML = "If Statements & Comparison Operators";
 
-let val;
+const id = 100;
 
-const today = new Date();
+//EQUAL TO
 
-val = today;
-console.log(typeof val);
+if (id == 100) {
+  console.log(`${id} does equal 100`)
+}
 
-val = today.toString();
-console.log(typeof val);
+if (id == 101) {
+  console.log(`${id} does equal 100`)
+} else {
+  console.log(`${id} equals 100 not 101`)
+}
 
-const birthday = new Date('9-10-1981 11:24:00');
-console.log(birthday);
+// NOT EQUAL TO
 
-let otherBirthday = new Date('September 10 1981');
-console.log(otherBirthday);
+if (id != 101) {
+  console.log('CORRECT');
+} else {
+  console.log('INCORRECT');
+}
 
-console.log(today.getMonth());
-console.log(today.getDate());
-console.log(today.getDay());
-console.log(today.getFullYear());
-console.log(today.getHours());
-console.log(today.getMinutes());
-console.log(today.getSeconds());
-console.log(today.getMilliseconds());
-console.log(today.getTime());
+// use === most times if you want to compare value and type
 
-let modBirthday = new Date('9/10/1981');
-console.log("modBirthday: " + modBirthday);
-modBirthday.setMonth(2);
-console.log("modBirthday: " + modBirthday);
-modBirthday.setDate(12);
-console.log("modBirthday: " + modBirthday);
-modBirthday.setFullYear(1985);
-console.log("modBirthday: " + modBirthday);
-modBirthday.setMinutes(30);
-console.log("modBirthday: " + modBirthday);
-modBirthday.setSeconds(25);
-console.log("modBirthday: " + modBirthday);
+const id_string = '100';
+
+if (id_string === '100') {
+  console.log('It is a string that is 100');
+}
+
+if (id_string == 100) {
+  console.log('CORRECT. 100 string equals 100 int with ==');
+}
+
+// test if symbol is defined
+if (typeof foo !== 'undefined') {
+  console.log(`The FOO id ${foo} `);
+} else {
+  console.log('NO FOO');
+}
+
+// GREATER THAN OR LESS THAN
+
+if (id > 200) {
+  console.log('ID is > 200');
+} else {
+  console.log('ID is < 200');
+}
+
+if (id >= 100) {
+  console.log('ID >= 100');
+}
+
+if (id <= 100) {
+  console.log('ID <= 100');
+}
+
+const color = 'red';
+
+if (color === 'chicken') {
+  console.log('Color is red');
+} else if (color === 'blue') {
+  console.log('Color is blue');
+} else {
+  console.log('Color is something we were not expecting');
+}
+
+// LOGICAL OPERATORS
+
+const personName = 'Steve';
+const age = 12;
+
+// && === AND
+if (age > 0 && age < 13) {
+  console.log(`${personName} is a child`);
+} else if (age >= 13 && age <= 19) {
+  console.log(`${personName} is a teenager`);
+} else {
+  console.log(`${personName} is an adult`);
+}
+
+// || === OR
+
+if (age < 16 || age > 65) {
+  console.log(`${personName} cannot run in race.`);
+} else {
+  console.log(`${personName} is registered for the race`);
+}
+
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// without braces.
+if (id === 100)
+  console.log('Works without braces which is madness');
+else
+  console.log('does not print, do not do this');
