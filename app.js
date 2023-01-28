f27169f4641494858f1c1fa86b6f79633f387291
Alan Scherger <alan.scherger@gmail.com>
2023-01-28 22:14:01 +0000

    General Loops

--- app.js
+++ app.js
@@ -1,63 +1,88 @@
 let header = document.getElementById('header');
-header.innerHTML = "Function Declarations and Expressions";
+header.innerHTML = "General Loops";
 
-// function declarations
+for (let i = 0; i < 10; i++) {
+  console.log(`Counter at ${i}`);
+}
 
-function greet(firstName, lastName) {
 
-  // ES5 mechanism
-  if (typeof firstName === 'undefined') {
-    firstName = 'defaultFirstName'
+for (let i = 0; i < 10; i++) {
+  if (i === 2) {
+    console.log('My favorite number is 2');
   }
-  if (typeof lastName === 'undefined') {
-    lastName = 'defaultLastName'
+  console.log(`Counter at ${i}`);
+}
+
+// continue to the next iteration
+for (let i = 0; i < 10; i++) {
+  if (i === 2) {
+    console.log('My favorite number is 2');
+    continue;
   }
-  // console.log('Hello');
-  return `Hello ${firstName} ${lastName}`;
+  console.log(`Counter at ${i}`);
 }
 
-function greetES6(firstName = 'defaultFirstName', lastname = 'defaultLastName') {
-  return `Hello ${firstName} ${lastname}`;
-}
-
-console.log(greet('Alan', 'Scherger'));
-console.log(greet());
-console.log(greetES6());
-
-// Function expressions
-const square = function(x = 3) {
-  return x * x;
-}
-
-console.log(square(9));
-
-// immediately invokable function expressions
-// IIFEs
-
-(function() {
-  console.log('IIFE Ran..');
-})();
-
-(function(name = 'Jon Doe') {
-  console.log(`Hello ${name}`);
-})('Brad');
-
-// Property Methods
-
-const todo = {
-  add: function(x) {
-    console.log(`add todo ${x}`);
-  },
-  edit: function(id) {
-    console.log(`edit ${id}`);
+for (let i = 0; i < 10; i++) {
+  if (i === 2) {
+    console.log('My favorite number is 2');
   }
+  if (i === 5) {
+    console.log('Time to stop at 5');
+    break;
+  }
+  console.log(`Counter at ${i}`);
 }
 
-todo.delete = function(id) {
-  console.log(`delete ${id} todo`);
+
+// WHILE
+
+let i = 0;
+while (i < 3) {
+  console.log(`while: ${i}`);
+  i++;
 }
 
-todo.add(33);
-todo.delete(33);
-todo.edit(44);
+i = 100;
+
+do {
+  console.log(`100 is not < 10 but it still prints: ${i}`);
+  i++;
+}
+while (i < 10);
+
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
+console.log('look at forEach')
+cars.forEach(function(car, index, array) {
+  console.log(`${car} :: ${index} :: ${array}`);
+});
+
+
+// MAP
+const users = [
+  { id: 1, name: 'John' },
+  { id: 2, name: 'Sara' },
+  { id: 3, name: 'Karen' },
+  { id: 4, name: 'Steve' },
+];
+
+const ids = users.map(function(user) {
+  return user.id;
+});
+
+console.log(ids);
+
+// for/in
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  age: 40,
+}
+
+for (let x in user) {
+  console.log(`${x} : ${user[x]}`);
+}
 
